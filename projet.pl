aad829a4901cfe7debb2d59aa2998bab550f3f6e
nicola169u <61577365+nicola169u@users.noreply.github.com>
2023-12-08 14:50:06 +0100

    fin mais il manque affichage des variables dans le menu

--- projet.pl
+++ projet.pl
@@ -1,21 +1,29 @@
+:- use_module(library(random)).
+:- dynamic echo_on/0.
+
 % Definition opérateur ?=
 :- op(20,xfy,?=).
 
 % Pour enlever les warning de Singleton variables
 :- style_check(-singleton).
 
-echo(T) :- echo_on, !, write(T).
+
 
 % QUESTION 1
 
+% occur_check/2 vérifie si une variable V n'apparaît pas dans la structure T.
+% Ce prédicat est récursif pour traiter les cas où T est un terme composé.
+% Si la variable V est trouvée dans la structure T, le prédicat échoue.
+
 occur_check(V, T) :-
-    var(V),
-    compound(T), 
-    arg(_, T, X), 
-    (V == X;
-    (compound(X), 
-    occur_check(V,X))),
-    !.
+    var(V),            % Vérifie si V est une variable
+    compound(T),       % Vérifie si T est un terme composé
+    arg(_, T, X),      % Récupère le sous-terme X de T
+    (V == X;           % Si V est égal à X, le prédicat réussit
+    (compound(X),      % Sinon, si X est un terme composé
+    occur_check(V, X))), % Applique récursivement occur_check sur X
+    !.                 % Utilise le cut pour éviter la recherche de solutions alternatives
+
 
 
 % règles pour la question 1
@@ -23,17 +31,17 @@ occur_check(V, T) :-
 
 
 regles(E, rename) :- 
-    arg(1, E, X),
-    arg(2, E, T),
-    var(X),
-    var(T).
+    arg(1, E, X),    % Récupère le premier argument de l'équation E dans la variable X
+    arg(2, E, T),    % Récupère le deuxième argument de l'équation E dans la variable T
+    var(X),          % Vérifie si X est une variable
+    var(T).          % Vérifie si T est une variable
 
 
 regles(E, simplify) :- 
-    arg(1, E, X),
-    arg(2, E, T),
-    atomic(T),
-    var(X).
+    arg(1, E, X),    % Récupère le premier argument de l'équation E dans la variable X
+    arg(2, E, T),    % Récupère le deuxième argument de l'équation E dans la variable T
+    atomic(T),       % Vérifie si T est atomic
+    var(X).          % Vérifie si X est une variable.
 
 
 regles(E, expand) :- 
@@ -213,35 +221,64 @@ unifie([], Strat) :-
     write("on est good pour la stratégie "),
     write(Strat),
     writeln("."),
-    !. % Condition d'arrêt
+    !.
 
 unifie([E|P], choix_premier) :-
+    print_systeme([E|P]),
     regles(E, R),
+    print_regles(R, E),
     reduit(R, E, P, Q),
     !,
     unifie(Q, choix_premier).
 
 unifie([E|P], choix_pondere_1) :- 
+    print_systeme([E|P]),
     regles(E, R),
+    print_regles(R, E),
     poids_max(P, Q, E, R, [], ponderation1),
     !, 
     unifie(Q, choix_pondere_1).
 
 
 unifie([E|P], choix_pondere_2) :- 
+    print_systeme([E|P]),
     regles(E, R), 
+    print_regles(R, E),
     poids_max(P, Q, E, R, [], ponderation2),
     !,
     unifie(Q, choix_pondere_2).
 
 
 unifie([E|P], choix_pondere_3) :- 
+    print_systeme([E|P]),
     regles(E, R), 
+    print_regles(R, E),
     poids_max(P, Q, E, R, [], ponderation3),
     !,
     unifie(Q, choix_pondere_3).
 
 
+% Prédicat unifie/2 avec choix aléatoire de règles
+% Unifie le système d'équations P en choisissant aléatoirement une équation et en appliquant la règle associée.
+% L'algorithme se poursuit récursivement jusqu'à ce que le système soit entièrement unifié.
+
+unifie(P, choix_aleatoire) :-
+    print_systeme(P),
+    % Calcul de la taille du système d'équations
+    length(P, Size), 
+    % Génération d'un indice aléatoire dans la plage de la taille du système
+    random(0, Size, Random), 
+    % Extraction d'une équation aléatoire (E) du système, avec le reste des équations (Reste)
+    nth0(Random, P, E, Reste),
+    % Obtention de la règle associée à l'équation
+    regles(E, R),
+    print_regles(R, E),
+    % Réduction du système en appliquant la règle à l'équation sélectionnée
+    reduit(R, E, Reste, Q), 
+    % Condition d'arrêt : le système est entièrement unifié
+    !, 
+    % Appel récursif avec le nouveau système Q
+    unifie(Q, choix_aleatoire).
 
 
 
@@ -280,14 +317,93 @@ meilleur_poids([E, F], X, Reste, Regle, Ponderation) :-
 
 
 
-
-
-
     
     
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% QUESTION 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
+% set_echo: ce prédicat active l'affichage par le prédicat echo
+set_echo :- 
+    assert(echo_on).
+
+% clr_echo: ce prédicat inhibe l'affichage par le prédicat echo
+clr_echo :- 
+    retractall(echo_on).
+
+% Predicat echo pour afficher les règles si l'écho est activé
+echo(Message) :-
+    ( echo_on -> write(Message); true ).
+
+
+% Prédicat unif/2 pour effectuer l'unification avec ou sans affichage des règles
+unif(P, S) :-
+    clr_echo,   % Inhiber l'écho à la fin de l'unification
+    unifie(P, S).
+
+
+% Prédicat trace_unif/2 pour effectuer l'unification avec affichage des règles à chaque étape
+trace_unif(P, S) :-
+    set_echo,   % Activer l'écho (affichage des règles)
+    unifie(P, S).
+
+
+% Prédicats pour laffichage                        
+print_systeme(P) :- 
+    echo('system : '), 
+    echo(P), 
+    echo('.\n').   
+
+
+print_regles(R,E) :- 
+    echo(R), 
+    echo(' : '), 
+    echo(E), 
+    echo('.\n').  
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% MENU %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Menu principal
+menu_principal :-
+    writeln('1. Entrer un système d''équations à unifier'),
+    writeln('2. Quitter'),
+    read(Choix),
+    writeln(Choix),
+    traiter_choix(Choix).
+
+
+
+traiter_choix(Choix) :- 
+    (Choix = 1 -> 
+        writeln('Entrez votre système d''équations à unifier (sous forme de liste, par exemple, [X ?= Y, Y ?= Z]):'),
+        read(Systeme),
+        writeln(Systeme),
+        writeln('Voulez-vous afficher le systeme et les règles à chaque itération de l\'unification ?'),
+        read(Affichage),
+        writeln("Quelle stratégie voulez-vous utiliser ?"),
+        writeln("1. Choix premier"),
+        writeln("2. Choix pondere 1"),
+        writeln("3. Choix pondere 2"),
+        writeln("4. Choix pondere 3"),
+        writeln("5. Choix alétoire"),
+        read(ChoixStrategie),
+        traiter_affichage(Affichage, Systeme, ChoixStrategie)
+    ; 
+        writeln("Au revoir !")
+    ).
+
+
+
+
+traiter_affichage(Affichage, Systeme, 1) :-
+    (Affichage = 'y' -> 
+        trace_unif(Systeme, choix_premier)
+    ; 
+        unif(Systeme, choix_premier)). 
 
 
 
